bd3d59d29ae78a38ed7c6bd80d044017f9cbf5db
masyasha <richello3027@gmail.com>
2016-03-23 09:56:10 +0300

    reBorn

--- Clear_Snake.py
+++ Clear_Snake.py
@@ -8,8 +8,37 @@ class Blocks():
         self.block_h = block_h
         self.block_x = random.randint(0,800)
         self.block_y = random.randint(0,600)
-        print self.block_x, self.block_y
+
+class Main():
+    def __init__(self, x_pos=400, y_pos=270):
+        self.x_pos = x_pos
+        self.y_pos = y_pos
+        x_change = y_change = 0
+        """ ### MAIN LOOP ### """
+        pygame.init()
+        display = pygame.display.set_mode((800,600))
+        displayColour = (30, 144, 255)
+        pygame.display.set_caption('Fresh Seva')
+        gameExit = False
+        while not gameExit:
+            for event in pygame.event.get():
+                if event.type == pygame.KEYDOWN:
+                    x_keys = {pygame.K_LEFT: x_change - 10,
+                            pygame.K_RIGHT: x_change + 10}
+                    y_keys = {pygame.K_DOWN: y_change - 10,
+                            pygame.K_UP: y_change + 10}
+
+                x_pos += x_change
+                y_pos += y_change
+                pygame.draw.rect(display, (0,0,0), [x_pos, y_pos, 20, 20])
+                display.fill(displayColour)
+
+                if event.type == pygame.QUIT:
+                        gameExit = True
+
+
 
 
 
 block = Blocks()
+main = Main()

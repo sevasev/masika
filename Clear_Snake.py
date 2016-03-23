327e1012988204b8578cd6b5feff6b27bc50291d
masyasha <richello3027@gmail.com>
2016-03-23 15:48:05 +0300

    reBorn

--- Clear_Snake.py
+++ Clear_Snake.py
@@ -3,55 +3,79 @@
 import pygame, random
 
 class Blocks():
-    def __init__(self, block_w=10, block_h=10):
+    def __init__(self, block_w=15, block_h=15):
         self.block_w = block_w
         self.block_h = block_h
         self.set_pos()
     def set_pos(self):
-        self.block_x = random.randint(0,800)
-        self.block_y = random.randint(0,600)
+        self.block_x = random.randint(0,50) * 15
+        self.block_y = random.randint(0,40) * 15
+
 
 class Main():
-    def __init__(self, x_pos=400, y_pos=270):
+    def __init__(self, x_pos=400, y_pos=280):
         self.x_pos, self.y_pos = x_pos, y_pos
         self.x_change = self.y_change = 0
         self.clock = pygame.time.Clock()
         self.main_loop()
     def main_loop(self, gameExit=False):
+        self.gameExit = gameExit
         """ ### MAIN LOOP ### """
         pygame.init()
-        display = pygame.display.set_mode((800,600))
-        displayColour = (245, 245, 220)
+        self.display = pygame.display.set_mode((750,600))
+        displayColour = (245, 230, 150)
         pygame.display.set_caption('Fresh Seva')
-        while not gameExit:
+        while not self.gameExit:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
-                    gameExit = True
+                    self.gameExit = True
                 """ SAVING FROM AN ERROR + MAIN ENGINE'S HERE """
                 try:
                     if event.type == pygame.KEYDOWN:
                         try:
-                            x_keys = {276: -1,
-                                        275: 1}
+                            x_keys = {276: -15,
+                                        275: 15}
                             self.x_change = x_keys[event.key]
                             self.y_change = 0
                         except:
-                            y_keys = {274: 1,
-                                        273: -1}
+                            y_keys = {274: 15,
+                                        273: -15}
                             self.y_change = y_keys[event.key]
                             self.x_change = 0
                 except:
                     print("An Error Occurs!")
-                    gameExit = True
+                    self.gameExit = True
 
-            self.x_pos += self.x_change if self.x_change <= 0 else 1
-            self.y_pos += self.y_change if self.y_change <= 0 else 1
-            display.fill(displayColour)
-            pygame.draw.rect(display, (0,0,0), [self.x_pos, self.y_pos, 15, 15])
+                """ DATA PROCESSING """
+            self.x_pos += self.x_change if abs(self.x_change) <= 15 else 15
+            self.y_pos += self.y_change if abs(self.y_change) <= 15 else 15
+            if abs(self.x_pos) <= 0 or abs(self.x_pos) >= 785:
+                self.lose()
+            if abs(self.y_pos) <= 0 or abs(self.y_pos) >= 585:
+                self.lose()
+            self.display.fill(displayColour)
+            """ CHEKING BLOCK'S STATUS """
+            if self.x_pos == bl.block_x:
+                if self.y_pos == bl.block_y:
+                    del(self.bl_drown)
+                    bl.set_pos()
+                    self.block_print()
+            else:
+                self.block_print()
+            pygame.draw.rect(self.display, (0,0,0), [self.x_pos, self.y_pos, 15, 15])
+
             pygame.display.update()
 
-            self.clock.tick(100)
+            self.clock.tick(20)
+
+    def lose(self):
+        print "YOU LOSE!"
+        self.gameExit = True
+
+    def block_print(self):
+        self.bl_drown = pygame.draw.rect(self.display, (0,244,15), [bl.block_x, bl.block_y, bl.block_w, bl.block_h])
 
 
-block = Blocks()
+
+bl = Blocks()
 main = Main()
